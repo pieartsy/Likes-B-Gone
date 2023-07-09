c895625670610402f4c8da5c374e3aa7ff50cdaa
pieartsy <17207322+pieartsy@users.noreply.github.com>
2023-07-08 23:22:02 -0400

    fixed bug

--- javascripts/discourse/initializers/init-hide-likes.js
+++ javascripts/discourse/initializers/init-hide-likes.js
@@ -1,23 +1,23 @@
 import { apiInitializer } from "discourse/lib/api";
 export default apiInitializer("0.8", (api) => {
 
-    api.reopenWidget("post-menu", {
+  api.reopenWidget("post-menu", {
 
-        getWhoLiked() {
-            const { attrs, state } = this;
-            return new Promise((resolve, reject) => {
-                state.likedUsers = [];
-                state.total = 0;
-                resolve();
-            })
-        },
-        attachButton(name) {
-            let button = this._super(name);
-            if(name === 'like') {
-                button.children[0].attrs.contents = '';
-                button.children[0].contents = '';
-            }
-            return button;
+    getWhoLiked() {
+        const { attrs, state } = this;
+        return new Promise((resolve, reject) => {
+            state.likedUsers = [];
+            state.total = 0;
+            resolve();
+        })
+    },
+    attachButton(name) {
+        let button = this._super(name);
+        if(name === 'like' && button?.children[0] != null) {
+            button.children[0].attrs.contents = '';
+            button.children[0].contents = '';
         }
-    });
+        return button;
+    }
+});
 });
